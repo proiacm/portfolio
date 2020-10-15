7a50a005dc9b8a981506794b2408de4cf3bbd805
proiacm <proiacm@gmail.com>
2020-10-15 13:07:58 -0600

    Play with css for Contact component

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            <a target='_blank' href='https://www.linkedin.com/in/cmproia/'>LinkedIn</a> |  <a target='_blank' href='https://github.com/proiacm'>GitHub</a> | <a target='_blank' href='https://dev.to/proiacm'>DEV</a> | <a target='_blank' href={"mailto:" + 'email'}>email</a>
+        <div className='Contact'>
+            <a target='_blank' href='https://www.linkedin.com/in/cmproia/'> Let's connect via LinkedIn</a> | <a target='_blank' href='https://github.com/proiacm'>Collaborate on GitHub</a> | <a target='_blank' href='https://dev.to/proiacm'>Check out my blog on DEV</a> | <a target='_blank' href={"mailto:" + 'email'}>email</a>
         </div>
     )
 }
