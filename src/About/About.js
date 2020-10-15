7c52356f29b005725273c9d36a6915e3dc932fdb
proiacm <proiacm@gmail.com>
2020-10-15 12:34:57 -0600

    Play with css for About component

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
+        <div className='About'>
            <h1>I'm CiaraMaria and I'm a Software Developer!</h1> 
            <p>I leverage empathy and experience in building client relationships while designing adaptive, aesthetic applications.</p>
         </div>
