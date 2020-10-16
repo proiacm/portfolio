9d632691e73c4f52ae3c85a95cf441603b1ae242
proiacm <proiacm@gmail.com>
2020-10-15 19:09:51 -0600

    More css fun!

--- src/About/About.js
+++ src/About/About.js
@@ -3,9 +3,11 @@ import './About.css';
 
 const About = () => {
     return (
-        <div className='About'>
-           <h1>I'm CiaraMaria and I'm a Software Developer!</h1> 
-           <p>I leverage empathy and experience in building client relationships while designing adaptive, aesthetic applications.</p>
+        <div className='About-img'>
+            <div className='About'>
+            <h1>I'm CiaraMaria and I'm a Software Developer!</h1> 
+            <p>I leverage empathy and experience in building client relationships while designing adaptive, aesthetic applications.</p>
+            </div>
         </div>
     )
 }
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,8 +3,10 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-        <div className='Contact'>
-            <a target='_blank' href='https://www.linkedin.com/in/cmproia/'> Let's connect via LinkedIn</a> | <a target='_blank' href='https://github.com/proiacm'>Collaborate on GitHub</a> | <a target='_blank' href='https://dev.to/proiacm'>Check out my blog on DEV</a> | <a target='_blank' href={"mailto:" + 'email'}>email</a>
+        <div className='Contact-img'>
+            <div className='Contact'>
+                <a target='_blank' href='https://www.linkedin.com/in/cmproia/'> Let's connect via LinkedIn</a> | <a target='_blank' href='https://github.com/proiacm'>Collaborate on GitHub</a> | <a target='_blank' href='https://dev.to/proiacm'>Check out my blog on DEV</a> | <a target='_blank' href={"mailto:" + 'email'}>email</a>
+            </div>
         </div>
     )
 }
